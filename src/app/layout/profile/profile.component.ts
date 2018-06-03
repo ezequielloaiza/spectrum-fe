37377c99eb41d70455d1eb1b4aaba029af688f3e
Eliezer Cardenas <eliezercardenas@bitbucket.org>
2018-06-02 23:28:30 -0400

    working profile

--- src/app/layout/profile/profile.component.ts
+++ src/app/layout/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder
+             ) { }
 
   ngOnInit() {
+    this.initializeForm();
   }
+
+  initializeForm() {
+    this.form = this.formBuilder.group({
+      name               : ['', [ Validators.required]],
+      email              : ['', [ Validators.required, Validators.pattern(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)]],
+      address            : [''],
+      state              : ['', [ Validators.required]],
+      country            : ['', [ Validators.required]],
+      city               : ['', [ Validators.required]],
+      postal             : ['', [ Validators.required]],
+    });
+  }
+
+  get name() { return this.form.get('name'); }
+  get email() { return this.form.get('email'); }
+  get address() { return this.form.get('address'); }
+  get city() { return this.form.get('city'); }
+  get state() { return this.form.get('state'); }
+  get country() { return this.form.get('country'); }
+  get postal() { return this.form.get('postal'); }
 
 }
