b1282a9d6a43f122c0f66abeb9a03435d1350ef9
AJHOSEMAR <AJHOSEMAR19@GMAIL.COM>
2018-06-12 22:34:43 -0400

    listado-vendedor

--- src/app/layout/seller/list-seller/list-seller.component.ts
+++ src/app/layout/seller/list-seller/list-seller.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../shared/services';
+import { AlertifyService } from '../../../shared/services/alertify/alertify.service';
+import { ToastrService } from 'ngx-toastr';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CodeHttp } from '../../../shared/enum/code-http.enum';
 
 @Component({
   selector: 'app-list-seller',
@@ -7,9 +12,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListSellerComponent implements OnInit {
 
-  constructor() { }
+  listSellers: Array<any> = new Array;
+  listSellersAux: Array<any> = new Array;
+
+  constructor(private userService: UserService,
+    private alertify: AlertifyService,
+    private notification: ToastrService,
+    private modalService: NgbModal) { }
 
   ngOnInit() {
+    this.getListSellers();
+  }
+
+  getListSellers(): void {
+    this.userService.findByRole$().subscribe(res => {
+      if (res.code === CodeHttp.ok) {
+        this.listSellers = res.data;
+        this.listSellersAux = res.data;
+      }
+    });
+  }
+
+  getItems(ev: any) {
+    this.listSellers = this.listSellersAux;
+
+    const val = ev.target.value;
+
+    if (val && val.trim() !== '') {
+      this.listSellers = this.listSellers.filter((item) => {
+        return ((item.name.toLowerCase().indexOf(val.toLowerCase()) > -1) ||
+        (item.email.toLowerCase().indexOf(val.toLowerCase()) > -1) ||
+        (item.country.toLowerCase().indexOf(val.toLowerCase()) > -1) ||
+        (item.city.toLowerCase().indexOf(val.toLowerCase()) > -1));
+      });
+    }
+  }
+
+  changeStatus(id): void {
+    this.alertify.confirm('Delete Shipping Address', 'Are you sure do you want to delete this register?', () => {
+      this.userService.changeStatus$(id).subscribe(res => {
+        this.notification.success('User save', 'Success');
+        this.getListSellers();
+      });
+    }, () => { });
+  }
+
+  openModal(): void {
+   /* const modalRef = this.modalService.open(UserModalComponent, { size: 'lg', windowClass: 'modal-content-border' });
+    modalRef.result.then((result) => {
+      this.getListSellers();
+    } , (reason) => {
+    });*/
   }
 
 }
+
+
