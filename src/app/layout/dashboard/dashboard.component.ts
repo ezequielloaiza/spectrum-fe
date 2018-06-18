9c4e180c9d549ef64e5960bf5c45c52cfea187a1
Eliezer Cardenas <eliezercardenas@bitbucket.org>
2018-06-18 06:34:21 -0400

    skeleton dashboard

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -81,9 +81,9 @@ export class DashboardComponent implements OnInit {
 
   // lineChart
   public lineChartData: Array<any> = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-    { data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C' }
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Pending Orders' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Paid Orders' },
+    { data: [18, 48, 77, 9, 100, 27, 40], label: 'Canceled Orders' }
   ];
   public lineChartLabels: Array<any> = [
     'January',
