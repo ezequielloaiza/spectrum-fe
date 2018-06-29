6b2b020d893f146bf7ee55773701842848bec8bf
Arturo Loaiza <you@example.com>
2018-06-29 11:16:19 -0400

    creating product order

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -13,6 +13,8 @@ export class ProductDetailComponent implements OnInit {
   product: any;
   parameters: any;
   valueCurrent: any;
+  quantity = 1;
+  order: any;
   constructor(private productComponent:ProductsComponent) { }
 
   ngOnInit() {
@@ -65,9 +67,41 @@ export class ProductDetailComponent implements OnInit {
     });
   }
 
+  buildOrder() {
+    var color = _.find(this.product.colors, function (color) { 
+      return color.selected === true;
+    });
+
+    var type = _.find(this.product.types, function (type) { 
+      return type.selected === true;
+    });
+
+    //verificar atributos del producto que se enviaran a la orden
+    var productToBuy = {
+      id: this.product.id,
+      name: this.product.name,
+      priceBase: this.product.priceBase,
+      price1: this.product.price1,
+      price2: this.product.price2,
+      price3: this.product.price3,
+      category: this.product.category,
+      subCategory: this.product.subCategory,
+      material: this.product.material,
+      description: this.product.description,
+      replacementPeriod: this.product.replacementPeriod,
+      warranty: this.product.warranty,
+      stock: this.product.stock,
+      color: color.name,
+      type,
+      quantityBuy: this.quantity
+    }
+    return productToBuy;
+  }
+
   
   buyNow() {
-    console.log(this.product);
+    this.order = this.buildOrder();
+    console.log(this.order);
 	}
 
 }
