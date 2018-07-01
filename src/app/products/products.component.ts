4f01b31dd0339aeb6267dabdfdfeae2a65de217d
Arturo Loaiza <you@example.com>
2018-06-30 21:48:25 -0400

    add tabs in product detail

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -34,58 +34,62 @@ export class ProductsComponent implements OnInit {
           {
             name: 'Spheric & Toric',
             parameters: [
-              { name: 'Diameter', type:"number", values: [ { rangeStart: 13.00, rangeEnd: 14.50, step: null } ], selected: 13.00 },
-              { name: 'Base Curve', type:"number", values: [ {rangeStart: 7.70, rangeEnd: 9.80, step: 0.30} ], selected: 7.70  },
-              { name: 'Axis Toric', type:"number", values: [ '(Pasos de 5°) Todos'] }, //TODO --> verificar rangos.
               { name: 'Addition', type:"radio", values: [
-                                                      { name: 'A', selected: true },
-                                                      { name: 'B', selected: false },
-                                                      { name: 'C', selected: false }]
+                { name: 'A', selected: true },
+                { name: 'B', selected: false },
+                { name: 'C', selected: false }]
               }, //TODO --> verificar si estos valores son por separado.
-              { name: 'Cylinder Toric', type:"number", values: [ {rangeStart: -0.80, rangeEnd: -0.75, step: 0.25} ], selected: -0.80  },
-              { name: 'Sphere', type:"number", values: [ {rangeStart: -30.00, rangeEnd: -30.00, step: 0.25} ], selected: -30.00  },
               { name: 'Multifocal Design', type:"radio", values: [
                                                                 { name: 'CD', selected: true },
                                                                 { name: 'CN', selected: false }]
-              }
+              },
+              { name: 'Diameter', type:"number", values: [ { rangeStart: 13.00, rangeEnd: 14.50, step: null } ], selected: 13.00 },
+              { name: 'Base Curve', type:"number", values: [ {rangeStart: 7.70, rangeEnd: 9.80, step: 0.30} ], selected: 7.70  },
+              { name: 'Axis Toric', type:"number", values: [ '(Pasos de 5°) Todos'] }, //TODO --> verificar rangos.
+              
+              { name: 'Cylinder Toric', type:"number", values: [ {rangeStart: -0.80, rangeEnd: -0.75, step: 0.25} ], selected: -0.80  },
+              { name: 'Sphere', type:"number", values: [ {rangeStart: -30.00, rangeEnd: -30.00, step: 0.25} ], selected: -30.00  },
+              
             ],
             selected: true
           },{
             name: 'Multifocal',
             parameters: [
-              { name: 'Diameter', type:"number", values: [ {rangeStart: 14.00, rangeEnd: 14.00, step: null} ], selected: 14.00 },
-              { name: 'Base Curve', type:"number", values: [ {rangeStart: 8.00, rangeEnd: 9.00, step: 0.20} ], selected: 8.00 },
-              { name: 'Axis Toric', type:"number", values: [ '(Pasos de 5°) Todos' ] },
               { name: 'Addition', type:"radio", values: [
-                                                      { name: 'A', selected: true },
-                                                      { name: 'B', selected: false },
-                                                      { name: 'C', selected: false }]
+                { name: 'A', selected: true },
+                { name: 'B', selected: false },
+                { name: 'C', selected: false }]
               },
-              { name: 'Cylinder Toric', type:"number", values: [ {rangeStart: -0.80, rangeEnd: -0.75, step: 0.25} ], selected: -0.80 },
-              { name: 'Sphere', type:"number", values: [ {rangeStart: -12.00, rangeEnd: 8.00, step: 0.25} ], selected: -12.00 },
               { name: 'Multifocal Design', type:"radio", values: [
                                                                 { name: 'CD', selected: true },
                                                                 { name: 'CN', selected: false }]
-              }
+              },
+              { name: 'Diameter', type:"number", values: [ {rangeStart: 14.00, rangeEnd: 14.00, step: null} ], selected: 14.00 },
+              { name: 'Base Curve', type:"number", values: [ {rangeStart: 8.00, rangeEnd: 9.00, step: 0.20} ], selected: 8.00 },
+              { name: 'Axis Toric', type:"number", values: [ '(Pasos de 5°) Todos' ] },
+              { name: 'Cylinder Toric', type:"number", values: [ {rangeStart: -0.80, rangeEnd: -0.75, step: 0.25} ], selected: -0.80 },
+              { name: 'Sphere', type:"number", values: [ {rangeStart: -12.00, rangeEnd: 8.00, step: 0.25} ], selected: -12.00 },
+             
             ],
             selected: false
           },{
             name: 'UV Blocking', //TODO --> verificar si viene anidado con el Spheryc & Toric
             parameters:  [
-              { name: 'Diameter', type:"number", values: [], selected: null }, //TODO --> cuando no tiene el parametro que rangos manejamos
-              { name: 'Base Curve', type:"number", values: [], selected: null },
-              { name: 'Axis Toric', type:"number", values: [ '(Pasos de 5°) Todos' ] },
               { name: 'Addition', type:"radio", values: [
-                                                      { name: 'A', selected: true },
-                                                      { name: 'B', selected: false },
-                                                      { name: 'C', selected: false }]
+                { name: 'A', selected: true },
+                { name: 'B', selected: false },
+                { name: 'C', selected: false }]
               },
-              { name: 'Cylinder Toric', type:"number", values: [ {rangeStart: -0.80, rangeEnd: -0.75, step: 0.25} ], selected: -0.80 },
-              { name: 'Sphere', type:"number", values: [], selected: null },
               { name: 'Multifocal Design', type:"radio", values: [
                                                                 { name: 'CD', selected: true },
                                                                 { name: 'CN', selected: false }]
-              }
+              },
+              { name: 'Diameter', type:"number", values: [], selected: null }, //TODO --> cuando no tiene el parametro que rangos manejamos
+              { name: 'Base Curve', type:"number", values: [], selected: null },
+              { name: 'Axis Toric', type:"number", values: [ '(Pasos de 5°) Todos' ] },
+              { name: 'Cylinder Toric', type:"number", values: [ {rangeStart: -0.80, rangeEnd: -0.75, step: 0.25} ], selected: -0.80 },
+              { name: 'Sphere', type:"number", values: [], selected: null },
+              
             ],
             selected: false
           }],
@@ -109,98 +113,103 @@ export class ProductsComponent implements OnInit {
           {
             name: 'Spheric & Toric & Rx Design',
             parameters: [
-              { name: 'Diameter', type:"number", values: [ {rangeStart: 14.40, rangeEnd: 14.40, step: null} ], selected: 14.40 },
-              { name: 'Base Curve', type:"number", values: [ {rangeStart: 8.70, rangeEnd: 8.70, step: null} ], selected: 8.70 },
-              { name: 'Axis Toric', type:"number", values: [], selected: null },
               { name: 'Addition', type:"radio", values: [
                                                       { name: 'Low', selected: true },
                                                       { name: 'High', selected: false }]
               },
-              { name: 'Cylinder Toric', type:"number", values: [], selected: null },
-              { name: 'Sphere', type:"number", values: [], selected: null  },
               { name: 'Multifocal Design', type:"radio", values: [
                                                                 { name: 'CD', selected: true },
                                                                 { name: 'CN', selected: false }]
-              }
+              },
+              { name: 'Diameter', type:"number", values: [ {rangeStart: 14.40, rangeEnd: 14.40, step: null} ], selected: 14.40 },
+              { name: 'Base Curve', type:"number", values: [ {rangeStart: 8.70, rangeEnd: 8.70, step: null} ], selected: 8.70 },
+              { name: 'Axis Toric', type:"number", values: [], selected: null },
+              { name: 'Cylinder Toric', type:"number", values: [], selected: null },
+              { name: 'Sphere', type:"number", values: [], selected: null  },
+              
             ],
             selected: true
           },{
             name: 'Multifocal',
             parameters: [
-              { name: 'Diameter', type:"number", values: [ {rangeStart: 14.30, rangeEnd: 14.30, step: null} ], selected: 14.30 },
-              { name: 'Base Curve', type:"number", values: [ {rangeStart: 8.60, rangeEnd: 8.60, step: null} ], selected: 8.60 },
-              { name: 'Axis Toric', type:"number", values: [], selected: null },
               { name: 'Addition', type:"radio", values: [
                                                       { name: 'Low', selected: true },
                                                       { name: 'High', selected: false }]
               },
+              { name: 'Multifocal Design', type:"radio", values: [
+                                                                { name: 'CD', selected: true },
+                                                                { name: 'CN', selected: false }]
+              },
+              { name: 'Diameter', type:"number", values: [ {rangeStart: 14.30, rangeEnd: 14.30, step: null} ], selected: 14.30 },
+              { name: 'Base Curve', type:"number", values: [ {rangeStart: 8.60, rangeEnd: 8.60, step: null} ], selected: 8.60 },
+              { name: 'Axis Toric', type:"number", values: [], selected: null },
               { name: 'Cylinder Toric', type:"number", values: [], selected: null },
               { name: 'Sphere', type:"number", values: [
                           {rangeStart: -10.00, rangeEnd: -6.00, step: 0.50},
                           {rangeStart: -6.00, rangeEnd: 4.00, step: 0.25},
                           {rangeStart: 4.00, rangeEnd: 10.00, step: 0.50}
                         ], selected: null },
-              { name: 'Multifocal Design', type:"radio", values: [
-                                                                { name: 'CD', selected: true },
-                                                                { name: 'CN', selected: false }]
-              }
+              
             ],
             selected: false
           },{//DE AQUI PA ABAJO TA LOCO SOLO SIRVE EL NAME
             name: 'Toric',
             parameters: [
-              { name: 'Diameter', type:"number", values: [] },
-              { name: 'Base Curve', type:"number", values: [] },
-              { name: 'Axis Toric', type:"number", values: [ '(Pasos de 5°) Todos' ] },
               { name: 'Addition', type:"radio", values: [
                                                       { name: 'A', selected: true },
                                                       { name: 'B', selected: false },
                                                       { name: 'C', selected: false }]
               },
-              { name: 'Cylinder Toric', type:"number", values: [ {rangeStart: -0.75, rangeEnd: -0.80, step: 0.25} ] },
-              { name: 'Sphere', type:"number", values: [] },
               { name: 'Multifocal Design', type:"radio", values: [
                                                                 { name: 'CD', selected: true },
                                                                 { name: 'CN', selected: false }]
-              }
+              },
+              { name: 'Diameter', type:"number", values: [] },
+              { name: 'Base Curve', type:"number", values: [] },
+              { name: 'Axis Toric', type:"number", values: [ '(Pasos de 5°) Todos' ] },
+              { name: 'Cylinder Toric', type:"number", values: [ {rangeStart: -0.75, rangeEnd: -0.80, step: 0.25} ] },
+              { name: 'Sphere', type:"number", values: [] },
+              
             ],
             selected: false
           },{//DE AQUI PA ABAJO TA LOCO SOLO SIRVE EL NAME
             name: 'Toric & Rx Design',
             parameters: [
-              { name: 'Diameter', type:"number", values: [] },
-              { name: 'Base Curve', type:"number", values: [] },
-              { name: 'Axis Toric', type:"number", values: [ '(Pasos de 5°) Todos' ] },
               { name: 'Addition', type:"radio", values: [
                                                       { name: 'A', selected: true },
                                                       { name: 'B', selected: false },
                                                       { name: 'C', selected: false }]
               },
-              { name: 'Cylinder Toric', type:"number", values: [ {rangeStart: -0.75, rangeEnd: -0.80, step: 0.25} ] },
-              { name: 'Sphere', type:"number", values: [] },
               { name: 'Multifocal Design', type:"radio", values: [
                                                                 { name: 'CD', selected: true },
                                                                 { name: 'CN', selected: false }]
-              }
+              },
+              { name: 'Diameter', type:"number", values: [] },
+              { name: 'Base Curve', type:"number", values: [] },
+              { name: 'Axis Toric', type:"number", values: [ '(Pasos de 5°) Todos' ] },
+              { name: 'Cylinder Toric', type:"number", values: [ {rangeStart: -0.75, rangeEnd: -0.80, step: 0.25} ] },
+              { name: 'Sphere', type:"number", values: [] },
+              
             ],
             selected: false
           },{//DE AQUI PA ABAJO TA LOCO SOLO SIRVE EL NAME
             name: 'Spheric',
             parameters: [
-              { name: 'Diameter', type:"number", values: [] },
-              { name: 'Base Curve', type:"number", values: [] },
-              { name: 'Axis Toric', type:"number", values: [ '(Pasos de 5°) Todos' ] },
               { name: 'Addition', type:"radio", values: [
                                                       { name: 'A', selected: true },
                                                       { name: 'B', selected: false },
                                                       { name: 'C', selected: false }]
               },
-              { name: 'Cylinder Toric', type:"number", values: [ {rangeStart: -0.75, rangeEnd: -0.80, step: 0.25} ] },
-              { name: 'Sphere', type:"number", values: [] },
               { name: 'Multifocal Design', type:"radio", values: [
                                                                 { name: 'CD', selected: true },
                                                                 { name: 'CN', selected: false }]
-              }
+              },
+              { name: 'Diameter', type:"number", values: [] },
+              { name: 'Base Curve', type:"number", values: [] },
+              { name: 'Axis Toric', type:"number", values: [ '(Pasos de 5°) Todos' ] },
+              { name: 'Cylinder Toric', type:"number", values: [ {rangeStart: -0.75, rangeEnd: -0.80, step: 0.25} ] },
+              { name: 'Sphere', type:"number", values: [] },
+              
             ],
             selected: false
           }],
