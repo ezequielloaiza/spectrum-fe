5169b1b797899418b245c2d1931c9c8e9a38b222
Jeniree Ramirez <jenireers@gmail.com>
2018-07-18 20:32:06 -0400

    fix profile change password

--- src/app/layout/profile/profile.component.ts
+++ src/app/layout/profile/profile.component.ts
@@ -127,7 +127,6 @@ export class ProfileComponent implements OnInit {
   }
 
   saveAccount(): void {
-    this.form.get('city').setValue(this.form.value.city.description);
     this.userService.changePassword$(this.form.value).subscribe(res => {
       if (res.code === CodeHttp.ok) {
         this.user.userResponse = res.data;
