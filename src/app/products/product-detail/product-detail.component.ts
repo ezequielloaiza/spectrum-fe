092a58b645e8ce783e60516b05a533716630bbb6
Arturo Loaiza <you@example.com>
2018-07-21 17:06:17 -0400

    configurando producto xtensa

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -19,15 +19,41 @@ export class ProductDetailComponent implements OnInit {
   parameters: any;
   quantity = 1;
   order: any;
+
+  //configuration XTENSA product
+  axesXtensa     : Array<any> = new Array;
+  spheresXtensa  : Array<any> = new Array;
+  cylindersXtensa: Array<any> = new Array;
+
   constructor(private productService:ProductService, private route: ActivatedRoute, public router: Router) { }
 
   ngOnInit() {
     this.setEyes();
     this.getProducts();
+
+    /* var product xtensa */
+    this.setAxesXtensa();
+    this.setSpheresXtensa();
+    this.setCylindersXtensa();
   }
 
   setEyes() {
-    this.eyes = [ { type: 'left', name: 'Eye Left' }, { type: 'right', name: 'Eye Right' }];
+    this.eyes = [ { type: 'right', name: 'Eye Right' }, { type: 'left', name: 'Eye Left' } ];
+  }
+
+  setAxesXtensa(){
+    this.axesXtensa = [ { "values": ["5º","10º","15º","20º","25º","30º","35º","40º","45º","50º","55º","60º","65º","70º","75º","80º","85º","90º","95º","100º","105º","110º","115º","120º","125º","130º","135º","140º","145º","150º","155º","160º","165º","170º","175º","180º"] },
+                        { "values": ["10º", "20º","30º","40º","50º","60º","70º","80º","90º","100º","110º","120º","130º","140º","150º","160º","170º","180º"] }];
+  }
+
+  setSpheresXtensa(){
+    this.spheresXtensa = [ { "values": [ "-8.00","-7.50","-7.00","-6.50","-6.00","-5.75","-5.50","-5.25","-5.00","-4.75","-4.50","-4.25","-4.00","-3.75","-3.50","-3.25","-3.00","-2.75","-2.50","-2.25","-2.00","-1.75","-1.50","-1.25","-1.00","-0.75","-0.50","-0.25","0.00","0.25","0.50","0.75","1.00","1.25","1.50","1.75","2.00","2.25","2.50","2.75","3.00","3.25","3.50","3.75","4.00","4.50","5.00","5.50","6.00"] },
+                           { "values": [ "-10.00","-9.50","-8.00","-8.50","-8.00","-7.50","-7.00","-6.50","-6.00","-5.75","-5.50","-5.25","-5.00","-4.75","-4.50","-4.25","-4.00","-3.75","-3.50","-3.25","-3.00","-2.75","-2.50","-2.25","-2.00","-1.75","-1.50","-1.25","-1.00","-0.75","-0.50","-0.25","0.00","0.25","0.50","0.75","1.00","1.25","1.50","1.75","2.00","2.25","2.50","2.75","3.00","3.25","3.50","3.75","4.00","4.50","5.00","5.50","6.00","6.50","7.00","7.50","8.00"] }];
+  }
+
+  setCylindersXtensa(){
+    this.cylindersXtensa = [ { "values": ["-2.75","-2.25","-1.75","-1.25","-0.75"] },
+                        { "values": ["-5.75","-5.25","-4.75","-4.25","-3.75","-3.25","-2.75","-2.25","-1.75","-1.25","-0.75"] }];
   }
 
   getProducts() {
@@ -78,11 +104,46 @@ export class ProductDetailComponent implements OnInit {
   changeSelect(typeEye, parameter, value) {
     if (typeEye === 'left') {
       parameter.selectedLeft = value;
+      if (this.product.name === "Xtensa" && parameter.name === 'Cylinder (D)'){
+        this.setValuesAxesXtensa(value);
+      }
     } else {
       parameter.selectedRight = value;
+      if (this.product.name === "Xtensa" && parameter.name === 'Cylinder (D)'){
+        this.setValuesAxesXtensa(value);
+      }
     }
   }
 
+  setValuesSpheresXtensa(value) {
+    var paramSpheres = _.find(this.parameters, ['name', 'Sphere (D)']);
+    if (!value) { //Xtensa without RXDesign
+      paramSpheres.values = this.spheresXtensa[0].values;
+    } else {
+      paramSpheres.values = this.spheresXtensa[1].values;
+    }
+  }
+
+  setValuesCylindersXtensa(value) {
+    var paramCylinders = _.find(this.parameters, ['name', 'Cylinder (D)']);
+    if (!value) { //Xtensa without RXDesign
+      paramCylinders.values = this.cylindersXtensa[0].values;
+    } else {
+      paramCylinders.values = this.cylindersXtensa[1].values;
+    }
+  }
+
+
+  setValuesAxesXtensa(value) {
+    var paramAxes = _.find(this.parameters, ['name', 'Axes (º)']);
+    if (parseFloat(value) <= -3.25) {
+      paramAxes.values = this.axesXtensa[0].values;
+    } else {
+      paramAxes.values = this.axesXtensa[1].values;
+    }
+  }
+
+  
   checkedRadio(typeEye, value) {
     if (typeEye === 'left') {
       if (value.selectedLeft === true)
@@ -94,8 +155,18 @@ export class ProductDetailComponent implements OnInit {
     return false;
   }
 
-  setValueRadio(typeEye, value, values) {
-    this.cleanValueRadio(typeEye, values);
+  setValueRadio(typeEye, value, parameter) {
+    if (parameter.name === "Rx Design" && value.name === 'Yes') {
+      this.setValuesSpheresXtensa(true);
+      this.setValuesCylindersXtensa(true);
+      this.setValuesAxesXtensa("-5.75");
+    } else {
+      this.setValuesSpheresXtensa(false);
+      this.setValuesCylindersXtensa(false);
+      this.setValuesAxesXtensa("-2.75");
+    }
+
+    this.cleanValueRadio(typeEye, parameter.values);
     if (typeEye === 'left') {
       value.selectedLeft = true;
     } else {
