1442a96cd83cf1338465ced34e172a95cf3e8a2b
nailymm <nailymu@gmail.com>
2018-07-26 17:33:29 -0400

    [CRUD Garantia]
    - Agregada conexion a servicios.
    
    this close #50

--- src/app/shared/services/warranty/warranty.service.ts
+++ src/app/shared/services/warranty/warranty.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WarrantyService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public save$(warranty): Observable<any> {
+    return this.http.post(environment.apiUrl + 'warranties/save', warranty);
+  }
+
+  public update$(warranty): Observable<any> {
+    return this.http.put(environment.apiUrl + 'warranties/update', warranty);
+  }
+
+  public findAll$(): Observable<any> {
+    return this.http.get(environment.apiUrl + 'warranties/findAll');
+  }
+
+  public removeById$(id): Observable<any> {
+    return this.http.delete(environment.apiUrl + 'warranties/removeById/' + id);
+  }
 }
