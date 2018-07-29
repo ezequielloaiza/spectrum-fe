e7c70dad6679ff6449533301d9cda02fa17ff41d
Arturo Loaiza <you@example.com>
2018-07-29 14:17:20 -0400

    ordening by folder products more

---
+++ src/app/products/json-products(new)/Xtensa/6pkXtensaMF-CN.ts
@@ -0,0 +1,58 @@
+
+this.products = [
+  { "code": "DB3", "name": "6pk Xtensa Multifocal CN",
+    "priceBase": 4000, "price1": 3500, "price2": 3600, "price3": 3700,
+    "father": "Xtensa",
+    "category": "Contact Lenses",
+    "subCategory": "MarkEnnovy Contact Lenses",
+    "material": "Hidrogel, Filcon IV I 55%",
+    "descriptionShort": "My description short by xtensa",
+    "description": "Xtensa is a monthly soft contact lens for all audiences and lifestyles manufactured with our patented micro-precision technology.  Its consistent comfort and vision clarity make it ideal for the most dynamic and carefree contact lens users.",
+    "replacementPeriod": "3 months",
+    "warranty": "One year",
+    "url": "http://spctinternational.com/wp-content/uploads/2017/11/XTENSA.pdf",
+    "stock": 10,
+    "types": [{
+        "name": "Multifocal",
+        "parameters": [
+          { "name": "Diameter", "type":"number", "values": [ "14.30" ], "selectedLeft": null, "selectedRight": null },
+          { "name": "Base Curve", "type":"number", "values": [" 8.60" ], "selectedLeft": null, "selectedRight": null },
+          { "name": "Sphere", "type":"number", "values": [ "-10.00","-9.50","-9.00","-8.50","-8.00","-7.50","-7.00","-6.50","-6.00","-5.75","-5.50","-5.25","-5.00","-4.75","-4.50","-4.25","-4.00","-3.75","-3.50","-3.25","-3.00","-2.75","-2.50","-2.25","-2.00","-1.75","-1.50","-1.25","-1.00","-0.75","-0.50","-0.25","0.00","0.25","0.50","0.75","1.00","1.25","1.50","1.75","2.00","2.25","2.50","2.75","3.00","3.25","3.50","3.75","4.00","4.50","5.00","5.50","6.00","6.50","7.00","7.50","8.00","8.50","9.00","9.50","10.00"], "selectedLeft": null, "selectedRight": null },
+          { "name": "Addition (D)", "type":"radio", "values": [
+            { "name": "Low", "selectedLeft": true, "selectedRight": true },
+            { "name": "High", "selectedLeft": false, "selectedRight": false }]
+          },
+          { "name": "Multifocal - Design", "type":"radio", "values": [
+                                { "name": "CD", "selectedLeft": true, "selectedRight": true },
+                                { "name": "CN", "selectedLeft": false, "selectedRight": false }]
+          }
+        ],
+        "selectedLeft": true,
+        "selectedRight": true
+      }],
+      "infoAditional": [
+        {
+          "name": "Characteristics" , "values": [
+                          {"title":"More Custom", "description":"Design flexibilities beyond other standard hydrogels."},
+                          {"title":"More Comfort", "description":"A high-performance Hydrogel (55%)."},
+                          {"title":"Vision Type", "description":"Active—clear vision for your lifestyle."}
+                          
+          ]
+        },
+        {
+          "name": "Technical Specifications", "values": [
+                                  {"title": "Material", "description": "Hydrogel, Filcon IV I 55%"},
+                                  {"title": "DK", "description": "19"},
+                                  {"title": "Handling Tint", "description": "Yes"},
+                                  {"title": "UV Blocking", "description": "No"},
+                                  {"title": "Base Curve (mm)", "description": "8.60"},
+                                  {"title": "ØT (mm)", "description": "14.30"},
+                                  {"title": "Sphere (D)", "description": "-10.00 to +10.00 (-6.00 to +4.00 Steps 0.25; -6.50 to -10.00 Steps 0.50; +4.50 to +10.00 Steps 0.50)"},
+                                  {"title": "Addition (D)", "description": "LOW – HIGH"},
+                                  {"title": "Design MF", "description": "CD – CN"}
+          ]
+        }
+      ]
+
+  }
+];
