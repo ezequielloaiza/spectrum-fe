b45529c09e018811c2ce0dfc9c93807cd4a77afe
Arturo Loaiza <you@example.com>
2018-08-15 09:15:52 -0400

    fix diameter in all json products

--- src/app/products/json-products(new)/Xtensa/3pkXtensaRx.ts
+++ src/app/products/json-products(new)/Xtensa/3pkXtensaRx.ts
@@ -19,7 +19,7 @@ this.products = [
           { "name": "Rx Design", "type": "radio", "values": [
                                                       { "name": "Yes", "selected": true }]
           },
-          { "name": "Diameter (mm)", "type": "number", "values": ["14.40"], "selected": null },
+          { "name": "Diameter (mm)", "type": "number", "values": ["14.4"], "selected": null },
           { "name": "Base Curve (mm)", "type":"number", "values": [ "8.70" ], "selected": null },
           { "name": "Sphere (D)", "eye": "right", "type":"number", "values": [ "-8.00","-7.50","-7.00","-6.50","-6.00","-5.75","-5.50","-5.25","-5.00","-4.75","-4.50","-4.25","-4.00","-3.75","-3.50","-3.25","-3.00","-2.75","-2.50","-2.25","-2.00","-1.75","-1.50","-1.25","-1.00","-0.75","-0.50","-0.25","0.00","0.25","0.50","0.75","1.00","1.25","1.50","1.75","2.00","2.25","2.50","2.75","3.00","3.25","3.50","3.75","4.00","4.50","5.00","5.50","6.00"], "selected": null },
           { "name": "Cylinder (D)", "eye": "right", "type": "number", "values": ["-2.75","-2.25","-1.75","-1.25","-0.75"], "selected": null },
