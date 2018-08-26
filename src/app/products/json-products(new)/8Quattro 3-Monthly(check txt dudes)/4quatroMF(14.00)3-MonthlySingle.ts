7e1543e65eabb4c88854875d2a4ee5bff3670588
Arturo Loaiza <you@example.com>
2018-08-25 22:11:27 -0400

    REFACTOR json en base al documento de parametros

---
+++ src/app/products/json-products(new)/8Quattro 3-Monthly(check txt dudes)/4quatroMF(14.00)3-MonthlySingle.ts
@@ -0,0 +1,55 @@
+this.products = [
+  {
+    "code": "ESN", "name": "Quattro MF (14.00) 3-Monthly Single",
+    "priceBase": 2000, "price1": 1500, "price2": 1600, "price3": 1700,
+    "father": "Quattro 3-Monthly",
+    "category": "Contact Lenses",
+    "subCategory": "MarkEnnovy Contact Lenses",
+    "material": "Hidrogel, Filcon I 1 49%",
+    "descriptionShort": "My description short by quattro",
+    "description": "Quattro is individually manufactured with the material mark’ennovy was founded on, Filcon I 1 49%. It is our classic line of lenses produced with the most advanced technology for mark’ennovy’s most experienced contact lens users.",
+    "replacementPeriod": "3 months",
+    "warranty": "One year",
+    "url": "http://spctinternational.com/wp-content/uploads/2017/11/QUATTRO.pdf",
+    "stock": 10,
+    "types": [
+      {
+        "name": "Multifocal",
+        "parameters": [
+          { "name": "Diameter (mm)", "type": "selected", "values": ["14.00"], "selected": null },
+          { "name": "Base Curve (mm)", "type": "selected", "values": ["8.00", "8.20", "8.40", "8.60", "8.80", "9.00", "9.20", "9.40"], "selected": null },
+          { "name": "Sphere (D)", "type": "selected", "values": ["-12.00", "-11.75", "-11.50", "-11.25", "-11.00", "-10.75", "-10.50", "-10.25", "-10.00", "-9.75", "-9.50", "-9.25", "-9.00", "-8.75", "-8.50", "-8.25", "-8.00", "-7.75", "-7.50", "-7.25", "-7.00", "-6.75", "-6.50", "-6.25", "-6.00", "-5.75", "-5.50", "-5.25", "-5.00", "-4.75", "-4.50", "-4.25", "-4.00", "-3.75", "-3.50", "-3.25", "-3.00", "-2.75", "-2.50", "-2.25", "-2.00", "-1.75", "-1.50", "-1.25", "-1.00"], "selected": null },
+          { "name": "Addition", "type": "selected", "values": ["1.00", "1.50", "2.00", "2.50", "3.00"], "selected": null },
+          { "name": "Dominancia", "type":"selected", "values": [], "selected": "N" }
+        ],
+        "eyeLeft": null,
+        "eyeRight": null
+      }
+    ],
+    "infoAditional": [
+      {
+        "name": "Characteristics", "values": [
+          { "title": "More Custom", "description": "Individually made for each eye in multiple designs." },
+          { "title": "More Comfort", "description": "Proven Hydrogel (49%)." },
+          { "title": "Vision Type", "description": "Classic—vision satisfaction you have grown to trust." }
+
+        ]
+      },
+      {
+        "name": "Technical Specifications", "values": [
+          { "title": "Material", "description": "Hydrogel, Filcon I 1 49%" },
+          { "title": "DK", "description": "15" },
+          { "title": "MPa", "description": "0.29" },
+          { "title": "Handling Tint", "description": "Yes" },
+          { "title": "UV Blocking", "description": "Yes" },
+          { "title": "Base Curve (mm)", "description": "7.70 to 9.80 Steps 0.30" },
+          { "title": "ØT (mm)", "description": "14.50 to 13.00" },
+          { "title": "Sphere (D)", "description": "-30.00 to +30.00 Steps 0.25" },
+          { "title": "Cylinder (D)", "description": "-0.75 to -8.00 Steps 0.25" },
+          { "title": "Axis (°)", "description": "All Steps 5°" },
+          { "title": "Addition (D)", "description": "A – B – C" }
+        ]
+      }
+    ]
+  }
+];
