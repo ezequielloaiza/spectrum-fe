0b32df39d968e74673cdbdc8ac6aae312f9dd5c9
Eliezer Cardenas <eliezercardenas@bitbucket.org>
2018-09-02 22:10:03 -0400

    show cart size

--- src/app/shared/modules/header/header.component.ts
+++ src/app/shared/modules/header/header.component.ts
@@ -2,56 +2,73 @@ import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { UserStorageService } from '../../../http/user-storage.service';
+import { BasketService } from '../../services/basket/basket.service';
+import { CodeHttp } from '../../enum/code-http.enum';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss']
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-    pushRightClass: string = 'push-right';
-    user: any;
+  pushRightClass: string = 'push-right';
+  user: any;
 
-    constructor(private translate: TranslateService, public router: Router, private userStorageService: UserStorageService) {
+  constructor(private translate: TranslateService,
+    public router: Router,
+    private userStorageService: UserStorageService,
+    private basketService: BasketService) {
 
-        this.translate.addLangs(['en', 'fr', 'ur', 'es', 'it', 'fa', 'de', 'zh-CHS']);
-        this.translate.use(this.userStorageService.getLanguage());
-        this.user = JSON.parse(userStorageService.getCurrentUser());
-        this.router.events.subscribe(val => {
-            if (
-                val instanceof NavigationEnd &&
-                window.innerWidth <= 992 &&
-                this.isToggled()
-            ) {
-                this.toggleSidebar();
-            }
-        });
-    }
+    this.translate.addLangs(['en', 'fr', 'ur', 'es', 'it', 'fa', 'de', 'zh-CHS']);
+    this.translate.use(this.userStorageService.getLanguage());
+    this.user = JSON.parse(userStorageService.getCurrentUser());
+    debugger
+    this.router.events.subscribe(val => {
+      if (
+        val instanceof NavigationEnd &&
+        window.innerWidth <= 992 &&
+        this.isToggled()
+      ) {
+        this.toggleSidebar();
+      }
+    });
+  }
 
-    ngOnInit() {}
+  ngOnInit() { }
 
-    isToggled(): boolean {
-        const dom: Element = document.querySelector('body');
-        return dom.classList.contains(this.pushRightClass);
-    }
+  isToggled(): boolean {
+    const dom: Element = document.querySelector('body');
+    return dom.classList.contains(this.pushRightClass);
+  }
 
-    toggleSidebar() {
-        const dom: any = document.querySelector('body');
-        dom.classList.toggle(this.pushRightClass);
-    }
+  toggleSidebar() {
+    const dom: any = document.querySelector('body');
+    dom.classList.toggle(this.pushRightClass);
+  }
 
-    rltAndLtr() {
-        const dom: any = document.querySelector('body');
-        dom.classList.toggle('rtl');
-    }
+  rltAndLtr() {
+    const dom: any = document.querySelector('body');
+    dom.classList.toggle('rtl');
+  }
 
-    onLoggedout() {
-        this.userStorageService.signOut();
-        this.router.navigateByUrl('signin');
-    }
+  onLoggedout() {
+    this.userStorageService.signOut();
+    this.router.navigateByUrl('signin');
+  }
 
-    changeLang(language: string) {
-        this.translate.use(language);
-        this.userStorageService.setLanguage(language);
-    }
+  changeLang(language: string) {
+    this.translate.use(language);
+    this.userStorageService.setLanguage(language);
+  }
+
+  /*
+  cartSize() {
+    this.basketService.allBasketByUser$(this.user.userResponse.idUser).subscribe(res => {
+      if (res.code === CodeHttp.ok) {
+        debugger
+        return res.data.length;
+      }
+    });;
+  }
+  */
 }
