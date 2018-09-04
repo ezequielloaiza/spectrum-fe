fa436a736f6b8ae78e372a5957372f21ed863f4c
nailymm <nailymu@gmail.com>
2018-09-04 18:44:05 -0400

    add download markennovy WIP

--- src/app/layout/details-order-supplier/supplier-markennovy/supplier-markennovy.component.ts
+++ src/app/layout/details-order-supplier/supplier-markennovy/supplier-markennovy.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ProductRequested } from '../../../shared/models/productrequested';
 import { FileProductRequested } from '../../../shared/models/fileproductrequested';
+import { FileProductRequestedService } from '../../../shared/services/fileproductrequested/fileproductrequested.service';
+import { CodeHttp } from '../../../shared/enum/code-http.enum';
+import { environment } from '../../../../../src/environments/environment';
+
+const URL = environment.apiUrl + 'fileProductRequested/downloadFile/';
 
 @Component({
   selector: 'app-supplier-markennovy',
@@ -11,12 +16,20 @@ export class SupplierMarkennovyComponent implements OnInit {
 
   @Input() lista: Array<ProductRequested>;
   @Input() files: Array<FileProductRequested>;
-  constructor() { }
+  constructor(private fileProductRequestedService: FileProductRequestedService) { }
 
   ngOnInit() {
   }
 
   downloadFile(item) {
-
+    this.fileProductRequestedService.downloadFile$(item.name).subscribe(res => {
+      if (res.code === CodeHttp.ok) {
+        console.log('Delete files');
+      } else {
+        console.log(res.errors[0].detail);
+      }
+    }, error => {
+      console.log('error', error);
+    });
   }
 }
