495879d527da7020b156c2844d86fea12b0f475c
Jeniree Ramirez <jenireers@gmail.com>
2018-09-03 22:47:41 -0400

    fix delete file merge

--- src/app/shared/modules/header/header.component.ts
+++ src/app/shared/modules/header/header.component.ts
@@ -11,53 +11,6 @@ import { CodeHttp } from '../../enum/code-http.enum';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-<<<<<<< HEAD
-    pushRightClass: string = 'push-right';
-    user: any;
-
-    constructor(private translate: TranslateService, public router: Router, private userStorageService: UserStorageService) {
-
-        this.translate.addLangs(['en', 'fr', 'ur', 'es', 'it', 'fa', 'de', 'zh-CHS']);
-        this.translate.use(this.userStorageService.getLanguage());
-        this.user = JSON.parse(userStorageService.getCurrentUser());
-        this.router.events.subscribe(val => {
-            if (
-                val instanceof NavigationEnd &&
-                window.innerWidth <= 992 &&
-                this.isToggled()
-            ) {
-                this.toggleSidebar();
-            }
-        });
-    }
-
-    ngOnInit() {}
-
-    isToggled(): boolean {
-        const dom: Element = document.querySelector('body');
-        return dom.classList.contains(this.pushRightClass);
-    }
-
-    toggleSidebar() {
-        const dom: any = document.querySelector('body');
-        dom.classList.toggle(this.pushRightClass);
-    }
-
-    rltAndLtr() {
-        const dom: any = document.querySelector('body');
-        dom.classList.toggle('rtl');
-    }
-
-    onLoggedout() {
-        this.userStorageService.signOut();
-        this.router.navigateByUrl('signin');
-    }
-
-    changeLang(language: string) {
-        this.translate.use(language);
-        this.userStorageService.setLanguage(language);
-    }
-=======
   pushRightClass: string = 'push-right';
   user: any;
 
@@ -106,5 +59,4 @@ export class HeaderComponent implements OnInit {
     this.translate.use(language);
     this.userStorageService.setLanguage(language);
   }
->>>>>>> develop
 }
