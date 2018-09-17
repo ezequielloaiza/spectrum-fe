d58fa37297c64c0e6430569fd224d63d16332c25
Jeniree Ramirez <jenireers@gmail.com>
2018-09-17 07:04:32 -0400

    [Delete option upload file MarKennovy] - Delete option upload file in components Markennovy

--- src/app/layout/details-order-supplier/supplier-markennovy/supplier-markennovy.component.ts
+++ src/app/layout/details-order-supplier/supplier-markennovy/supplier-markennovy.component.ts
@@ -1,32 +1,19 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ProductRequested } from '../../../shared/models/productrequested';
-import { FileProductRequested } from '../../../shared/models/fileproductrequested';
-import { FileProductRequestedService } from '../../../shared/services/fileproductrequested/fileproductrequested.service';
-import { CodeHttp } from '../../../shared/enum/code-http.enum';
 import { environment } from '../../../../../src/environments/environment';
-import { saveAs } from 'file-saver';
-
-const URL = environment.apiUrl + 'fileProductRequested/downloadFile/';
 
 @Component({
   selector: 'app-supplier-markennovy',
   templateUrl: './supplier-markennovy.component.html',
   styleUrls: ['./supplier-markennovy.component.scss']
+
 })
 export class SupplierMarkennovyComponent implements OnInit {
 
   @Input() lista: Array<ProductRequested>;
-  @Input() files: Array<FileProductRequested>;
-  constructor(private fileProductRequestedService: FileProductRequestedService) { }
+
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  downloadFile(item) {
-    this.fileProductRequestedService.downloadFile$(item.name).subscribe(res => {
-      saveAs(res);
-    }, error => {
-      console.log('error', error);
-    });
   }
 }
