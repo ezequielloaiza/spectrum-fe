d1a61b692a88b574311921253670390043837dd8
jenireers <jenireers@gmail.com>
2018-10-10 22:01:52 +0000

    [Fix modal user]

--- src/app/layout/user/modals/user-modal/user-modal.component.ts
+++ src/app/layout/user/modals/user-modal/user-modal.component.ts
@@ -37,6 +37,7 @@ export class UserModalComponent implements OnInit {
                        { id: 1, name: 'Postpaid' }];
   listCreditDays = [ '15', '30', '60' ];
   postpaid = false;
+  msjPayment = true;
 
   constructor(private modal: NgbActiveModal,
     private formBuilder: FormBuilder,
@@ -226,7 +227,7 @@ export class UserModalComponent implements OnInit {
       this.form.get('creditDays').setValue(0);
       this.form.get('creditLimit').setValue(0);
     }
-
+    this.msjPayment = false;
     this.form.get('paymentMethod').setValue(method.id);
   }
 }
