dd65dc0be8897d2ae9c336c0f996ca39dac0c8e5
Arturo Loaiza <arloaiza4@hotmail.com>
2018-10-14 18:09:38 +0000

    Feature/#177 list by suppliers and magic look

---
+++ src/app/products/json-products(new-06102018)/magicLook.ts
@@ -0,0 +1,70 @@
+this.products = [
+  {
+    "code": "", "name": "Magic Look",
+    "priceBase": 6000, "price1": 5500, "price2": 5600, "price3": 5700,
+    "father": "Magic Look",
+    "category": "Contact Lenses",
+    "subCategory": "MarkEnnovy Contact Lenses",
+    "material": "Silicone Hydrogel, Filcon V3 75%",
+    "descriptionShort": "My description short by euclid",
+    "description": "Saphir represents a family of monthly and 3-monthly soft contact lenses individually manufactured with Silicone Hydrogel, the most advanced material on the market, which maximises oxygen flow to the eye. Its unique combination of comfort, breathability and vision quality suits even the most demanding contact lens user.",
+    "replacementPeriod": "Monthly",
+    "warranty": "90",
+    "url": "http://spctinternational.com/contacts/",
+    "stock": 10,
+    "types": [
+      {
+        "name": "",
+        "parameters": [
+          { "name": "Base Curve (mm)", "type": "selected", "values": ["8.6"], "selected": "8.6" },
+          { "name": "Diameter (mm)", "type": "selected", "values": ["14.5"], "selected": "14.5" }
+        ],
+        "parametersBoxes": [
+          { "name": "Tone", "type": "selected", "values": ["1 TONE", "2 TONE", "3 TONE"], "selected": null },
+          { "name": "Color", "type": "image", "values": [ [
+                                                            {"image": "assets/images/products/magiclook/1TONE-1.png", "value":"GRAY"},
+                                                            {"image": "assets/images/products/magiclook/1TONE-2.png", "value":"BROWN"},
+                                                            {"image": "assets/images/products/magiclook/1TONE-3.png", "value":"GREEN"},
+                                                            {"image": "assets/images/products/magiclook/1TONE-4.png", "value":"TRUE SAPPHIRE"}
+                                                          ],
+                                                          [
+                                                            {"image": "assets/images/products/magiclook/2TONE-1.png", "value":"GRAY"},
+                                                            {"image": "assets/images/products/magiclook/2TONE-2.png", "value":"BROWN"},
+                                                            {"image": "assets/images/products/magiclook/2TONE-3.png", "value":"GREEN"},
+                                                            {"image": "assets/images/products/magiclook/2TONE-4.png", "value":"TURQUOISE"},
+                                                            {"image": "assets/images/products/magiclook/2TONE-5.png", "value":"TRUE SAPPHIRE"},
+                                                            {"image": "assets/images/products/magiclook/2TONE-6.png", "value":"AMETHYST"},
+                                                            {"image": "assets/images/products/magiclook/2TONE-7.png", "value":"HONEY"},
+                                                            {"image": "assets/images/products/magiclook/2TONE-8.png", "value":"HAZEL"},
+                                                            {"image": "assets/images/products/magiclook/2TONE-9.png", "value":"TRUE HAZEL"}
+                                                          ],
+                                                          [
+                                                            {"image": "assets/images/products/magiclook/3TONE-1.png", "value":"SUGAR GRAY"},
+                                                            {"image": "assets/images/products/magiclook/3TONE-2.png", "value":"DARK GRAY"},
+                                                            {"image": "assets/images/products/magiclook/3TONE-3.png", "value":"JADE GREEN"},
+                                                            {"image": "assets/images/products/magiclook/3TONE-4.png", "value":"GREEN"},
+                                                            {"image": "assets/images/products/magiclook/3TONE-5.png", "value":"AQUA MARIN"},
+                                                            {"image": "assets/images/products/magiclook/3TONE-6.png", "value":"PURE HAZEL"},
+                                                            {"image": "assets/images/products/magiclook/3TONE-7.png", "value":"TOPAZ BLUE"},
+                                                            {"image": "assets/images/products/magiclook/3TONE-8.png", "value":"BLUE"},
+                                                            {"image": "assets/images/products/magiclook/3TONE-9.png", "value":"SWEET VIOLET"},
+                                                            {"image": "assets/images/products/magiclook/3TONE-10.png", "value":"CHESNUT"},
+                                                            {"image": "assets/images/products/magiclook/3TONE-11.png", "value":"HONEY"}
+                                                          ]
+                                                        ], "selected": null }
+        ]
+      }
+    ],
+    "infoAditional": [
+      {
+        "name": "Properties", "values": [
+          { "title": "Toric", "image": "assets/images/products/magiclook/1TONE-1.png", "value":"GRAY" },
+          { "title": "Multifocal", "image": "assets/images/products/magiclook/1TONE-1.png", "value":"GRAY" },
+          { "title": "3-Monthly", "image": "assets/images/products/magiclook/1TONE-1.png", "value":"GRAY" },
+          { "title": "Sylicone Hydrogel", "image": "assets/images/products/magiclook/1TONE-1.png", "value":"GRAY" },
+          { "title": "UV Filter", "image": "assets/images/products/magiclook/1TONE-1.png", "value":"GRAY" }
+        ]
+      }
+    ]
+  }
+];
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
 import { ProductsListsComponent } from './products-lists/products-lists.component';
+import { ProductsListInternalComponent } from './products-lists-internal/products-lists-internal.component';
 import { ProductViewComponent } from './product-view/product-view.component';
 import { ProductViewEuropaComponent } from './product-view-europa/product-view-europa.component';
 import { TranslateModule } from '@ngx-translate/core';
@@ -37,6 +38,7 @@ import { NotificationBalanceComponent } from './modals/notification-balance/noti
   ],
   declarations: [
     ProductsComponent,
+    ProductsListInternalComponent,
     ProductsListsComponent,
     ProductViewComponent,
     ProductViewEuropaComponent,
