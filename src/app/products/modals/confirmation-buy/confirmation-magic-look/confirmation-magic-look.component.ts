b2af40d63e42449370f5b14952b99173ae805561
jenireers <jenireers@gmail.com>
2018-11-14 11:58:15 +0000

    [Fix products]

--- src/app/products/modals/confirmation-buy/confirmation-magic-look/confirmation-magic-look.component.ts
+++ src/app/products/modals/confirmation-buy/confirmation-magic-look/confirmation-magic-look.component.ts
@@ -95,8 +95,8 @@ export class ConfirmationMagicLookComponent implements OnInit {
   }
 
   save(): void {
+    this.spinner.show();
     if (this.typeBuy === 1) {
-      this.spinner.show();
       this.basketRequest.idUser = this.datos.idUser;
       this.basketRequest.productRequestedList = this.lista;
       this.basketRequest.fileProductRequestedList = this.listFileBasket;
@@ -104,7 +104,7 @@ export class ConfirmationMagicLookComponent implements OnInit {
         if (res.code === CodeHttp.ok) {
             this.save_success = true;
             this.close();
-            this.translate.get('Successfully saved', {value: 'Successfully saved'}).subscribe(( res: string) => {
+            this.translate.get('Successfully Saved', {value: 'Successfully Saved'}).subscribe(( res: string) => {
               this.notification.success('', res);
             });
             this.spinner.hide();
