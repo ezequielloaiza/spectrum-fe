0aa1a4ca1cc0380080237950ea139a6e7efc936b
Jhosemar <ajhosemar19@gmail.com>
2018-11-15 23:45:07 +0000

    acomodando json euclid para topaz y saphir toric

--- src/app/products/json-products(new-06102018)/euclid/3saphirToric.ts
+++ src/app/products/json-products(new-06102018)/euclid/3saphirToric.ts
@@ -10,7 +10,7 @@ this.products = [
     "description": "Saphir represents a family of monthly and 3-monthly soft contact lenses individually manufactured with Silicone Hydrogel, the most advanced material on the market, which maximises oxygen flow to the eye. Its unique combination of comfort, breathability and vision quality suits even the most demanding contact lens user.",
     "replacementPeriod": "Monthly",
     "warranty": "90",
-    "url": "http://spctinternational.com/wp-content/uploads/2017/11/SAPHIR.pdf",
+    "url": "",
     "stock": 10,
     "types": [
       {
@@ -20,10 +20,11 @@ this.products = [
           { "name": "Steep K", "type": "input", "values": [], "placeholder": "e.g. 05.00", "selected": null },
           { "name": "HVID", "type": "input", "values": [], "placeholder": "e.g. 03.00", "selected": null },
           { "name": "Axes (º)", "type": "input", "values": [], "placeholder": "e.g. 008", "selected": null },
-          { "name": "Sphere (D)", "type": "input", "values": [], "placeholder": "e.g. -25.00", "selected": null },
+          { "name": "Sphere (D)", "type": "selected", "values": ["-8.00","-7.75","-7.50","-7.25","-7.00","-6.75","-6.50","-6.25","-6.00","-5.75","-5.50","-5.25","-5.00","-4.75","-4.50","-4.25","-4.00",
+                                                                 "-3.75","-3.50","-3.25","-3.00","-2.75","-2.50","-2.25","-2.00","-1.75","-1.50","-1.25","-1.00","-0.75","-0.50","-0.25","0.00"],"selected": null },
           { "name": "Cylinder (D)", "type": "selected", "values": ["-3.00","-2.75","-2.50","-2.25","-2.00","-1.75"], "selected": null },
 
-          
+
           { "name": "Warranty", "type": "selected", "values": ["Yes", "No"], "selected": null }
         ],
         "eyeLeft": null,
@@ -39,7 +40,7 @@ this.products = [
           { "title": "Sylicone Hydrogel", "image": "assets/images/products/magiclook/1TONE-1.png", "value":"GRAY" },
           { "title": "UV Filter", "image": "assets/images/products/magiclook/1TONE-1.png", "value":"GRAY" }
         ]
-      }, 
+      },
       {
         "name": "Prices", "values": [
           { "title": "Warranty", "values":[
