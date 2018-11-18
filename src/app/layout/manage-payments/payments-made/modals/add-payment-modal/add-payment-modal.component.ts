15796bcd3387e01a2488f1db127fab118dab1933
Eliezer Cardenas <eliezer.cardenas@cleteci.com>
2018-11-18 16:38:07 -0400

    add payment information

--- src/app/layout/manage-payments/payments-made/modals/add-payment-modal/add-payment-modal.component.ts
+++ src/app/layout/manage-payments/payments-made/modals/add-payment-modal/add-payment-modal.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup } from '@angular/forms';
+import { FileUploader } from 'ng2-file-upload';
+import { FileProductRequested } from '../../../../../shared/models/fileproductrequested';
+import { environment } from '../../../../../../environments/environment';
 
+const URL = environment.apiUrl + 'fileProductRequested/uploader';
 @Component({
   selector: 'app-add-payment-modal',
   templateUrl: './add-payment-modal.component.html',
@@ -7,9 +13,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPaymentModalComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  listTypes = ['Transfer', 'Deposit', 'Check'];
+
+  queueLimit = 5;
+  maxFileSize = 25 * 1024 * 1024; // 25 MB
+  listFileBasket: Array<FileProductRequested> = new Array;
+  private uploadResult: any = null;
+  public uploader: FileUploader = new FileUploader({url: URL,
+                                                    itemAlias: 'files',
+                                                    queueLimit: this.queueLimit,
+                                                    maxFileSize: this.maxFileSize,
+                                                    removeAfterUpload: false,
+                                                    autoUpload: false});
+
+  constructor(public modalReference: NgbActiveModal) { }
 
   ngOnInit() {
+  }
+
+  close() {
+    this.modalReference.close();
   }
 
 }
