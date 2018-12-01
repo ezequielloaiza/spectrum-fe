d2d6533692830dcdc717cde26f12a1bb2a8b2f1f
Arturo Loaiza <arturo.loaiza@cleteci.com>
2018-12-01 14:40:02 -0400

    construct jsons fluo trips

---
+++ src/app/products/json-products(current 01Dic2018)/supplies/HiGlo(High Molecular).ts
@@ -0,0 +1,24 @@
+this.products = [
+  {
+    "code": "", "name": "HiGlo (High Molecular)",
+    "priceBase": 16, "price1": 16, "price2": 16, "price3": 16,
+    "descriptionShort": "My description short by fluo 2",
+    "infoAditional": [
+      {
+        "name": "Properties", "values": [
+          "When fitting soft disposable or soft specialized contact lenses; the high molecular fluorescein becomes your best allied thanks to its stainless effect.",
+          "User friendly and convenient to use formula, which generates a premium fluorescence effect. Mostly utilized on preliminary eye diagnosis.",
+          "Your fittings will be more precise from now on!",
+          "Packaging: 100 Strips"
+        ]
+      }
+    ]
+  }
+];
+
+/* 
+<i class="fas fa-eye"></i>
+<i class="fas fa-flag-checkered"></i>
+<i class="fas fa-trophy"></i>
+<i class="fas fa-edit"></i>
+*/
---
+++ src/app/products/json-products(current 01Dic2018)/supplies/RegularFluorescein.ts
@@ -0,0 +1,24 @@
+this.products = [
+  {
+    "code": "", "name": "Regular Flurorescein",
+    "priceBase": 8, "price1": 8, "price2": 8.50, "price3": 8,
+    "descriptionShort": "My description short by fluo 1",
+    "infoAditional": [
+      {
+        "name": "Properties", "values": [
+          "The #1 trusted brand worldwide manufactured with the highest quality standards.",
+          "Helps identify corneal and conjunctival deformities, thus guaranteeing outstanding results.",
+          "Unique formula, that facilitates the process of gas permeable contact lens fitting, as well as new generations of specialized contact lenses such as hybrid and scleral.",
+          "Packaging: 100 Strips"
+        ]
+      }
+    ]
+  }
+];
+
+/* 
+<i class="fas fa-trophy"></i>
+<i class="fas fa-times-circle"></i>
+<i class="fas fa-flask"></i>
+<i class="fas fa-edit"></i>
+*/
