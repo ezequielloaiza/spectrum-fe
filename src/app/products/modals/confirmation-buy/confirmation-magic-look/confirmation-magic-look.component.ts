de51bb7e8d805155640a9d44be3eab59ec6eded5
Jhosemar <ajhosemar19@gmail.com>
2018-12-07 01:03:02 +0000

    agregando imagenes y acomodando spinner en la confirmacion

--- src/app/products/modals/confirmation-buy/confirmation-magic-look/confirmation-magic-look.component.ts
+++ src/app/products/modals/confirmation-buy/confirmation-magic-look/confirmation-magic-look.component.ts
@@ -95,8 +95,8 @@ export class ConfirmationMagicLookComponent implements OnInit {
   }
 
   save(): void {
-    this.spinner.show();
     if (this.typeBuy === 1) {
+      this.spinner.show();
       this.basketRequest.idUser = this.datos.idUser;
       this.basketRequest.productRequestedList = this.lista;
       this.basketRequest.fileProductRequestedList = this.listFileBasket;
@@ -127,6 +127,7 @@ export class ConfirmationMagicLookComponent implements OnInit {
       this.buyNow.fileProductRequestedList = this.listFileBasket;
       this.validateAvailableBalance();
       if (this.available) {
+        this.spinner.show();
         this.orderService.saveOrderDirect$(this.buyNow).subscribe(res => {
         if (res.code === CodeHttp.ok) {
           this.save_success = true;
