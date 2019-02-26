2e207c7075db8048c3c278cfdf7f57e964f367a2
Jhosemar <ajhosemar19@gmail.com>
2019-02-26 03:00:32 +0000

    Merge branch 'feature/#363-Product-jade' of https://gitlab.com/Softguaro/spectrum-fe into feature/#363-Product-jade

---
+++ src/app/products/json-products(current 01Dic2018)/jade/jadeBlister.ts
@@ -0,0 +1,41 @@
+
+this.products = [
+  { "code":"JAB", "name": "Jade Blister",
+    "priceBase": 0, "price1": 0, "price2": 0, "price3": 0,
+    "father": "Jade",
+    "category": "Contact Lenses",
+    "subCategory": "MarkEnnovy Contact Lenses",
+    "material": "Hidrogel(52% H2O) - Filcon IV I ",
+    "descriptionShort": "My description short by jade",
+    "description": "Jade is a monthly lens that combines class I UV filter and blue light selective blocking filter to protect the eyes from 99% of UVB light, 93% of UVA light and 14% of blue-violet light harmful.",
+    "replacementPeriod": "Monthly",
+    "warranty": "",
+    "url": "",
+    "stock": 10,
+    "types": [
+      {
+        "name": "Aspheric",
+        "parameters": [
+          { "name": "Diameter (mm)", "type": "selected", "values": ["14.40"], "selected": null },
+          { "name": "Base Curve (mm)", "type":"selected", "values": [ "8.70" ], "selected": null },
+          { "name": "Sphere (D)", "type":"selected", "values": [ "-10.00","-9.50","-9.00","-8.50","-8.00","-7.50","-7.00","-6.50","-6.00","-5.75","-5.50","-5.25","-5.00","-4.75","-4.50","-4.25","-4.00","-3.75","-3.50","-3.25","-3.00","-2.75","-2.50","-2.25","-2.00","-1.75","-1.50","-1.25","-1.00","-0.75","-0.50","-0.25","0.00","0.25","0.50","0.75","1.00","1.25","1.50","1.75","2.00","2.25","2.50","2.75","3.00","3.25","3.50","3.75","4.00","4.25","4.50","4.75","5.00","5.25","5.50","5.75","6.00","6.50","7.00","7.50","8.00"], "selected": null }
+        ],
+        "eyeLeft": null,
+        "eyeRight": null
+      }],
+    "infoAditional": [
+      {
+        "name": "Properties",
+        "values": [
+          { "title": "k2", "image": ""},
+          { "title": "Aspheric", "image": "assets/images/properties/aspheric.png"},
+          { "title": "Toric", "image": "assets/images/properties/toric.png"},
+          { "title": "Multifocal", "image": "assets/images/properties/multifocal.png"},
+          { "title": "Monthly", "image": "assets/images/properties/monthly.png"},
+          { "title": "Hydrogel", "image": ""},
+          { "title": "RX DESIGN", "image": ""}
+        ]
+      }
+    ]
+  }
+];
