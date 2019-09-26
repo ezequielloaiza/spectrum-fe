bdae39488edd205e43a148c209bdc12d6b38eb98
Eliezer Cardenas <eliezer.cardenas@cleteci.com>
2019-09-26 17:09:30 -0400

    changed ip to domain

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://54.209.228.182:8443/api/v1/',
+  apiUrl: 'https://www.myspectrumorders.com:8443/api/v1/',
   firebase: {
     apiKey: 'AIzaSyDueIh6nxo-wxTG2moQbq_iNDKFFtNDARI',
       authDomain: 'spectrum-app.firebaseapp.com',
