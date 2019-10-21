d97b0c950e198b601640dc84f6b4ce07d655cfd9
Arturo Loaiza <arturo.loaiza@cleteci.com>
2019-10-20 20:08:05 -0400

    more changes, validation filter date

--- src/app/layout/invoice-client-qbo/invoice-client-qbo.component.ts
+++ src/app/layout/invoice-client-qbo/invoice-client-qbo.component.ts
@@ -3,6 +3,9 @@ import { InvoiceClientService } from '../../shared/services';
 import { saveAs } from 'file-saver';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { error } from 'protractor';
+import * as _ from 'lodash';
+import { NgbDateStruct, NgbDatepicker } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-invoice-client-qbo',
@@ -13,11 +16,14 @@ export class InvoiceClientQBOComponent implements OnInit {
 
   listInvoices: Array<any> = new Array;
   listInvoicesAux: Array<any> = new Array;
+  model: NgbDateStruct;
+  fechaSelecOrd: NgbDatepicker;
   
   constructor(private invoiceClientService: InvoiceClientService, private spinner: NgxSpinnerService) { }
 
   ngOnInit() {
     this.getInvoiceClientQBO();
+    this.model = { year: 0, month: 0, day: 0 };
   }
 
   getInvoiceClientQBO() {
@@ -60,11 +66,41 @@ export class InvoiceClientQBOComponent implements OnInit {
       this.listInvoices = this.listInvoices.filter((item) => {
         return ((item.number.toLowerCase().indexOf(val.toLowerCase()) > -1) ||
           (item.docNumber.toLowerCase().indexOf(val.toLowerCase()) > -1) ||
-          (item.date.toLowerCase().indexOf(val.toLowerCase()) > -1) ||
-          (item.dueDate.toLowerCase().indexOf(val.toLowerCase()) > -1) ||
           (item.qboTotalAmt.toLowerCase().indexOf(val.toLowerCase()) > -1));
       });
     }
+  }
+
+  getFecha(date): String {
+    let ano;
+    let mes;
+    let dia;
+    let fecha: String;
+    // Ano
+    ano = date.year;
+    // Mes
+    date.month < 10 ? mes = '0' + date.month : mes = date.month;
+    // Dia
+    date.day < 10 ? dia = '0' + date.day : dia = date.day;
+    // FechaFiltro
+    fecha = ano + '-' + mes + '-' + dia;
+    return fecha;
+  }
+
+  filterDate(value): void {
+    const lista = [];
+    let filterDate: String;
+    filterDate = this.getFecha(value);
+
+    this.listInvoices = this.listInvoices.filter((item) => {
+      let dateList: String;
+      let dueDateList: String;
+      dateList = _.toString(item.date.slice(0, 10));
+      dueDateList = _.toString(item.dueDate.slice(0, 10));
+
+      // tslint:disable-next-line:radix
+      return _.isEqual(filterDate, dateList) || _.isEqual(filterDate, dueDateList);
+    });
   }
 
 }
