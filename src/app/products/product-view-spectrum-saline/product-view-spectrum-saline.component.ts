1173ee7c9c8a5158cff140640a01a1ab17e11382
Eliézer Cárdenas <eliezercardenas17@gmail.com>
2019-11-19 12:32:56 +0000

    feature/#471-additional-info-customers

--- src/app/products/product-view-spectrum-saline/product-view-spectrum-saline.component.ts
+++ src/app/products/product-view-spectrum-saline/product-view-spectrum-saline.component.ts
@@ -153,6 +153,12 @@ export class ProductViewSpectrumSalineComponent implements OnInit {
           this.listCustomersAux = res.data;
           this.listCustomers = this.listCustomersAux;
           this.excludeClients(this.listCustomers, this.product.supplier.idSupplier, this.product);
+          //this.listCustomers.map((i) => { i.fullName = i.accSpct + ' ' + i.country.name + ' ' + i.name; return i; });
+          this.listCustomers.map((i) => {
+            let accSpct = !!i.accSpct ?  i.accSpct + ' ' : '';
+            i.fullName = accSpct + i.name + ', ' + i.country.name;
+            return i;
+          });
         }
       });
     }
