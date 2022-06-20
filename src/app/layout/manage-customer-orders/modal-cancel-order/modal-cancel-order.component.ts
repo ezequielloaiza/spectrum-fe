76d8508b3216be4fb1852659fec0ee7c3e9aa0ba
Arturo Loaiza <arloaiza4@gmail.com>
2022-06-20 02:42:03 -0400

    fix comments

--- src/app/layout/manage-customer-orders/modal-cancel-order/modal-cancel-order.component.ts
+++ src/app/layout/manage-customer-orders/modal-cancel-order/modal-cancel-order.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CodeHttp } from '../../../shared/enum/code-http.enum';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { OrderService } from '../../../shared/services/order/order.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-modal-cancel-order',
@@ -22,6 +23,10 @@ export class ModalCancelOrderComponent implements OnInit {
 
   close() {
     this.modalReference.close();
+  }
+
+  isValid() {
+    return !!_.trim(this.reasonCancel);
   }
 
   cancelOrder(): void {
